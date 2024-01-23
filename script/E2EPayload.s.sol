c06df85853024a849018b218eecd433e3e137cfd
sakulstra <lukasstrassel@googlemail.com>
2024-01-23 10:52:35 +0100

    fix: remove out dir

--- script/E2EPayload.s.sol
+++ script/E2EPayload.s.sol
@@ -1,48 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script, console} from 'forge-std/Script.sol';
 import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
 import {ProtocolV3TestBase, ReserveConfig, IPool} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Metis} from 'aave-address-book/AaveV3Metis.sol';
 import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 import {AaveV3Optimism} from 'aave-address-book/AaveV3Optimism.sol';
+import {AaveV3Arbitrum} from 'aave-address-book/AaveV3Arbitrum.sol';
+import {AaveV3BNB} from 'aave-address-book/AaveV3BNB.sol';
+import {AaveV3Scroll} from 'aave-address-book/AaveV3Scroll.sol';
+import {AaveV3PolygonZkEvm} from 'aave-address-book/AaveV3PolygonZkEvm.sol';
+import {AaveV3Avalanche} from 'aave-address-book/AaveV3Avalanche.sol';
 import {ChainIds} from 'aave-helpers/ChainIds.sol';
 
-
 contract E2EPayload is Script, ProtocolV3TestBase {
-    function run(uint40 payloadId) public {
-        IPool pool = _getPool();
-        if(address(pool) == address(0)) return;
-        defaultTest(vm.toString(payloadId),pool, payloadId, true);
-    }
+  function run(uint40 payloadId) public {
+    IPool pool = _getPool();
+    if (address(pool) == address(0)) return;
+    defaultTest(vm.toString(payloadId), pool, payloadId, true);
+  }
 
-    function defaultTest(
-        string memory reportName,
-        IPool pool,
-        uint40 payloadId,
-        bool runE2E
-    ) public returns (ReserveConfig[] memory, ReserveConfig[] memory) {
-        string memory beforeString = string(abi.encodePacked(reportName, '_before'));
-        ReserveConfig[] memory configBefore = createConfigurationSnapshot(beforeString, pool);
+  function defaultTest(
+    string memory reportName,
+    IPool pool,
+    uint40 payloadId,
+    bool runE2E
+  ) public returns (ReserveConfig[] memory, ReserveConfig[] memory) {
+    string memory beforeString = string(abi.encodePacked(reportName, '_before'));
+    ReserveConfig[] memory configBefore = createConfigurationSnapshot(beforeString, pool);
 
-        GovV3Helpers.executePayload(vm, payloadId);
+    GovV3Helpers.executePayload(vm, payloadId);
 
-        string memory afterString = string(abi.encodePacked(reportName, '_after'));
-        ReserveConfig[] memory configAfter = createConfigurationSnapshot(afterString, pool);
+    string memory afterString = string(abi.encodePacked(reportName, '_after'));
+    ReserveConfig[] memory configAfter = createConfigurationSnapshot(afterString, pool);
 
-        _diffReports(payloadId, beforeString, afterString);
+    _diffReports(payloadId, beforeString, afterString);
 
-        configChangePlausibilityTest(configBefore, configAfter);
+    configChangePlausibilityTest(configBefore, configAfter);
 
-        if (runE2E) e2eTest(pool);
-        return (configBefore, configAfter);
-    }
+    if (runE2E) e2eTest(pool);
+    return (configBefore, configAfter);
+  }
 
-    function _diffReports(uint40 payloadId, string memory reportBefore, string memory reportAfter) internal {
+  function _diffReports(
+    uint40 payloadId,
+    string memory reportBefore,
+    string memory reportAfter
+  ) internal {
     string memory outPath = string(
-      abi.encodePacked('./reports/payloads/', vm.toString(block.chainid),'/',vm.toString(address(GovV3Helpers.getPayloadsController(block.chainid))),'/',vm.toString(payloadId), '_forge.md')
+      abi.encodePacked(
+        './reports/payloads/',
+        vm.toString(block.chainid),
+        '/',
+        vm.toString(address(GovV3Helpers.getPayloadsController(block.chainid))),
+        '/',
+        vm.toString(payloadId),
+        '_forge.md'
+      )
     );
     string memory beforePath = string(abi.encodePacked('./reports/', reportBefore, '.json'));
     string memory afterPath = string(abi.encodePacked('./reports/', reportAfter, '.json'));
@@ -58,12 +74,15 @@ contract E2EPayload is Script, ProtocolV3TestBase {
     vm.ffi(inputs);
   }
 
-    function _getPool() internal returns (IPool pool) {
-        if(block.chainid == ChainIds.MAINNET) return AaveV3Ethereum.POOL;
-        if(block.chainid == ChainIds.OPTIMISM) return AaveV3Optimism.POOL;
-        if(block.chainid == ChainIds.METIS) return AaveV3Metis.POOL;
-        if(block.chainid == ChainIds.POLYGON) return AaveV3Polygon.POOL;
-    }
-
-
+  function _getPool() internal returns (IPool pool) {
+    if (block.chainid == ChainIds.MAINNET) return AaveV3Ethereum.POOL;
+    if (block.chainid == ChainIds.OPTIMISM) return AaveV3Optimism.POOL;
+    if (block.chainid == ChainIds.ARBITRUM) return AaveV3Arbitrum.POOL;
+    if (block.chainid == ChainIds.METIS) return AaveV3Metis.POOL;
+    if (block.chainid == ChainIds.POLYGON) return AaveV3Polygon.POOL;
+    if (block.chainid == ChainIds.AVALANCHE) return AaveV3Avalanche.POOL;
+    if (block.chainid == ChainIds.BNB) return AaveV3BNB.POOL;
+    if (block.chainid == ChainIds.ZK_EVM) return AaveV3PolygonZkEvm.POOL;
+    if (block.chainid == ChainIds.SCROLL) return AaveV3Scroll.POOL;
+  }
 }
