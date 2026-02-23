14c396ae93416d7a3fc3da35cfeefe07b71239a9
Lukas <lukasstrassel@googlemail.com>
2026-02-23 19:57:30 +0100

    fix: update helpers

--- src/tenderly.ts
+++ src/tenderly.ts
@@ -226,5 +226,5 @@ export async function simulateOnTenderly({
 }
 
 function flagAsKnown(value: string, reference: string) {
-  return `${value} [:ghost:](https://github.com/bgd-labs/aave-address-book  "${reference}")`;
+  return `${value} [:ghost:](https://github.com/aave-dao/aave-address-book  "${reference}")`;
 }
